7bf5974936a54bfb45547fb4f646189e0da9a458
JavariaNadeem81 <nadeemjavaria2@gmial.com>
2025-05-12 17:00:28 +0500

    modified

--- secret_code.py
+++ secret_code.py
@@ -10,3 +10,17 @@ class Greeting:
 # Creating an instance of the Greeting class and calling the method:
 greeting_instance = Greeting()
 print(greeting_instance.get_greeting_message())
+
+class Greeting:
+    def __init__ (self, defualt_greeting="Hello"):
+        self.defualt_greeting = defualt_greeting
+        
+    def get_greeting_message(self):
+        greeting_message = input("Enter your greeting message (defualt: {self.defualt}): ") or self.defualt_greeting
+        User_name = input("Enter your name:") or "User"
+        return f"{greeting_message}, {User_name}!"
+    
+#creating an instance of the greeting class and call the method:
+greeting_instance = Greeting()
+print(greeting_instance.get_greeting_message())
+        
